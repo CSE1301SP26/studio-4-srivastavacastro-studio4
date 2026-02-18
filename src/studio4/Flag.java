671d55b649c794258970df34920bd372ec0a435c
shreyastava <s.srivastava@wustl.edu>
2026-02-18 14:13:41 -0600

    Studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,23 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(86,0,124);
+		StdDraw.filledRectangle(.5, 0.5, 0.3, 0.2);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.setPenRadius(0.01);
+		StdDraw.rectangle(.5, 0.5, 0.3, 0.2);
+		StdDraw.setPenColor(StdDraw.WHITE);
+		StdDraw.filledCircle(0.45,0.55,0.01);
+		StdDraw.filledCircle(0.55,0.55,0.01);
+		StdDraw.filledCircle(0.5,0.5,.02);
+		StdDraw.setPenColor(StdDraw.WHITE);
+		StdDraw.setPenRadius(0.005);
+		StdDraw.arc(0.45,0.56,0.03,35,145);
+		StdDraw.arc(0.55,0.56,0.03,35,145);
+		StdDraw.text(0.5,0.35,"Minerva Grimace Land");
+		//StdDraw.line(0.65,0.85,0.2,0.45);
+		StdDraw.line(0.35,0.45,0.65,0.45);
+		StdDraw.line(0.35,0.45,0.5,0.67);
+		StdDraw.line(0.65,0.45,0.5,0.67);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,19 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+
 		
 	}
 }
